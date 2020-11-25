f0dedc71383f3737efd090694f4c5fac83c05735
Virendra patel <patelvirendra62@gmail.com>
2020-11-25 20:54:32 +0530

    Update registration_form.py

--- courses/forms/registration_form.py
+++ courses/forms/registration_form.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=20 , required = True)
+    last_name = forms.CharField(max_length=20 , required = True)
+    email = forms.EmailField(max_length=25 , required = True)
     class Meta:
         model = User
-        fields = ['first_name' , 'last_name' , 'username' , "email" , 'password1'  , 'password2' ]
+        fields = ['first_name'
+         , 'last_name' , 'username'
+          , "email" , 'password1'  , 'password2' ]
