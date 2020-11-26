4cb5473846a5cdc61268bcb02f369eb680a57239
Virendra patel <patelvirendra62@gmail.com>
2020-11-26 08:50:37 +0530

    ...

--- courses/forms/registration_form.py
+++ courses/forms/registration_form.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms import ValidationError
 
 class RegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=20 , required = True)
@@ -11,3 +12,15 @@ class RegistrationForm(UserCreationForm):
         fields = ['first_name'
          , 'last_name' , 'username'
           , "email" , 'password1'  , 'password2' ]
+        
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user  = None
+        try:
+            user = User.objects.get(email = email)
+        except:
+            return email
+            
+        if(user is not None):
+            raise ValidationError("User Already Exists");
+        
--- courses/views/auth.py
+++ courses/views/auth.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from courses.models import Course , Video
 from django.shortcuts import HttpResponse
 # Create your views here.
@@ -9,10 +9,16 @@ from courses.forms import RegistrationForm
 def signup(request ):
     if(request.method == "GET"):
         form = RegistrationForm()
-        return  render(request , 
+        return render(request , 
         template_name="courses/signup.html" , context= { 'form' : form } )    
     
-    return HttpResponse("POST REQUEST")
+    form = RegistrationForm(request.POST)
+    if(form.is_valid()):
+        user = form.save()
+        if(user is not None):
+            return redirect('login')
+    return render(request , 
+        template_name="courses/signup.html" , context= { 'form' : form } )
     
 
 def login(request ):
