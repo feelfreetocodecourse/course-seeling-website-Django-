6c5d31a8d2212c1462f6b946e3eacd72e2d72ef7
Virendra patel <patelvirendra62@gmail.com>
2021-01-14 11:36:09 +0530

    ...

--- courses/templatetags/course_custom_tags.py
+++ courses/templatetags/course_custom_tags.py
@@ -1,5 +1,7 @@
 from django import template
 import math
+
+from courses.models import UserCourse , Course 
 register = template.Library()
 
 # 100 -> 10% --> mrp  - ( mrp * discount * 0.01 ) = selprice
@@ -15,3 +17,21 @@ def cal_sellprice(price , discount):
 @register.filter
 def rupee(price):
     return f'₹{price}'
+
+
+
+
+@register.simple_tag
+def is_enrolled(request , course):
+   
+    user = None
+    if not request.user.is_authenticated:
+        return False
+        # i you are enrooled in this course you can watch every video
+    user = request.user
+    try:
+        user_course = UserCourse.objects.get(user = user  , course = course)
+        return True
+    except:
+        return False
+
--- courses/views/checkout.py
+++ courses/views/checkout.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , redirect
-from courses.models import Course , Video , Payment
+from courses.models import Course , Video , Payment , UserCourse
 from django.shortcuts import HttpResponse
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
@@ -19,33 +19,43 @@ def checkout(request , slug):
     action = request.GET.get('action')
     order = None
     payment = None
+    error = None
     if action == 'create_payment':
-        amount =  int((course.price - ( course.price * course.discount * 0.01 )) * 100)
-        currency = "INR"
-        notes = {
-            "email" : user.email, 
-            "name" : f'{user.first_name} {user.last_name}'
-        }
-        reciept = f"codewithvirendra-{int(time())}"
-        order = client.order.create(
-            {'receipt' :reciept , 
-            'notes' : notes , 
-            'amount' : amount ,
-            'currency' : currency
-            }
-        )
 
-        payment = Payment()
-        payment.user  = user
-        payment.course = course
-        payment.order_id = order.get('id')
-        payment.save()
+        try:
+           user_course = UserCourse.objects.get(user = user  , course = course)
+           error = "You are Already Enrolled in this Course"
+        except:
+            pass
+        
+        if error is None : 
+            amount =  int((course.price - ( course.price * course.discount * 0.01 )) * 100)
+            currency = "INR"
+            notes = {
+                "email" : user.email, 
+                "name" : f'{user.first_name} {user.last_name}'
+            }
+            reciept = f"codewithvirendra-{int(time())}"
+            order = client.order.create(
+                {'receipt' :reciept , 
+                'notes' : notes , 
+                'amount' : amount ,
+                'currency' : currency
+                }
+            )
+
+            payment = Payment()
+            payment.user  = user
+            payment.course = course
+            payment.order_id = order.get('id')
+            payment.save()
 
     context = {
         "course" : course , 
         "order" : order, 
         "payment" : payment, 
-        "user" : user
+        "user" : user , 
+        "error" : error
     }
     return  render(request , template_name="courses/check_out.html" , context=context )    
 
@@ -55,8 +65,24 @@ def verifyPayment(request):
     if request.method == "POST":
         data = request.POST
         context = {}
+        print(data)
         try:
             client.utility.verify_payment_signature(data)
+            razorpay_order_id = data['razorpay_order_id']
+            razorpay_payment_id = data['razorpay_payment_id']
+
+            payment = Payment.objects.get(order_id = razorpay_order_id)
+            payment.payment_id  = razorpay_payment_id
+            payment.status =  True
+            
+            userCourse = UserCourse(user = payment.user , course = payment.course)
+            userCourse.save()
+
+            print("UserCourse" ,  userCourse.id)
+
+            payment.user_course = userCourse
+            payment.save()
+
             return render(request , template_name="courses/my_courses.html" , context=context)    
 
         except:
