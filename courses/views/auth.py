ab099cd567f1f1d51eac4f78b3c31475b70f3c95
Virendra patel <patelvirendra62@gmail.com>
2021-01-14 19:34:02 +0530

    Update auth.py

--- courses/views/auth.py
+++ courses/views/auth.py
@@ -4,8 +4,19 @@ from django.shortcuts import HttpResponse
 from django.contrib.auth import logout
 from courses.forms import RegistrationForm , LoginForm
 from django.views import View
+from django.views.generic.edit import FormView
 
 
+class SignupView(FormView):
+    template_name="courses/signup.html" 
+    form_class = RegistrationForm
+    success_url  = '/login'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+'''
 class SignupView(View):
     def get(self , request):
         form = RegistrationForm()
@@ -21,7 +32,7 @@ class SignupView(View):
                 return redirect('login')
         return render(request , 
             template_name="courses/signup.html" , context= { 'form' : form } )
-    
+'''   
 
 class LoginView(View):
     def get(self , request):
